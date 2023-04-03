6cbed17025e1befb2ad3c255ec80cfd40cc391ff
Alana <115494193+alana1789@users.noreply.github.com>
2023-04-03 16:20:36 -0300

    1
    1

--- WindowsFormsPrincipal/Modelss/Funcionario.cs
+++ WindowsFormsPrincipal/Modelss/Funcionario.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Modelss
 {
-    internal class Funcionario
+    public class Funcionario
     {
     }
 }
--- WindowsFormsPrincipal/Modelss/Permissao.cs
+++ WindowsFormsPrincipal/Modelss/Permissao.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Modelss
 {
-    internal class Permissao
+    public class Permissao
     {
     }
 }
