aebe7e601e16b6cbb7b7bfcefc7cf4157e8689d9
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-04-04 16:37:47 -0300

    metodo dos funcionario BLL
    metodo dos na classes BLL do funcionario
    Inserir
    alterar
    excluir
    buscarPorId
    BuscarPorNomeFuncionario

--- WindowsFormsPrincipal/DALL/FuncionarioDAL.cs
+++ WindowsFormsPrincipal/DALL/FuncionarioDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace DAL
 {
     public class FuncionarioDAL
     {
+        public void Alterar(Funcionario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuscarPorNomeFuncionario(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
