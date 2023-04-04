13fa84946bed90fc6d0a309410b99cd9940bdd17
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-04-04 17:00:56 -0300

    Criação dos métodos

--- WindowsFormsPrincipal/BLL/GrupoFuncionarioBLL.cs
+++ WindowsFormsPrincipal/BLL/GrupoFuncionarioBLL.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using DAL;
+using Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL
 {
     public class GrupoFuncionarioBLL
     {
+        public void Inserir(GrupoFuncionario _grupoFuncionario)
+        {
+            GrupoFuncionarioDAL grupoFuncionarioDAL = new GrupoFuncionarioDAL();
+            grupoFuncionarioDAL.Inserir(_grupoFuncionario);
+        }
+        public void Alterar(GrupoFuncionario _grupoFuncionario)
+        {
+            GrupoFuncionarioDAL grupoFuncionarioDAL = new GrupoFuncionarioDAL();
+            grupoFuncionarioDAL.Alterar(_grupoFuncionario);
+        }
+        public void Excluir(int _id)
+        {
+            new GrupoFuncionarioDAL().Excluir(_id);
+        }
+        public List<GrupoFuncionario> BuscarTodos()
+        {
+            return new GrupoFuncionarioDAL().BuscarTodos();
+        }
+        public GrupoFuncionario BuscarPorId(int _id)
+        {
+            return new GrupoFuncionarioDAL().BuscarPorId(_id);
+        }
+        public List<GrupoFuncionario> BuscarPorNomeGrupo(string _nomeGrupo)
+        {
+            return new GrupoFuncionarioDAL().BuscarPorNomeGrupo(_nomeGrupo);
+        }
     }
 }
