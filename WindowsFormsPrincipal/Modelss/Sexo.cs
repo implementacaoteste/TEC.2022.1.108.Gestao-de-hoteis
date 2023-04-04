a631930b7ce4178b3406bc9028591680bab05d18
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-04-04 16:16:53 -0300

    criaçao da classes sexo models

---
+++ WindowsFormsPrincipal/Modelss/Sexo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelss
+{
+    public class Sexo
+    {
+        public int Id { get; set; }
+        public string Genero { get; set; }
+    }
+}
