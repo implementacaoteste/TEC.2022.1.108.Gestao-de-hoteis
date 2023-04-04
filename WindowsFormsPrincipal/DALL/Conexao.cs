0c97f6858f9aa6a1b40838c0a5f41a2279ac6707
Alana <115494193+alana1789@users.noreply.github.com>
2023-04-04 15:15:58 -0300

    04/04/2023
    (04/04/2023) 1º

--- WindowsFormsPrincipal/DALL/Conexao.cs
+++ WindowsFormsPrincipal/DALL/Conexao.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DAL
+﻿namespace DAL
 {
-    public class Conexao
+    public static class Conexao
     {
+        public static string StringDeConexao
+        {
+            get
+            {
+                return "User ID=SA;Initial Catalog=Configuracao;Data Source=.\\SQLEXPRESS2019;Password=Senailab02";
+            }
 
+        }   
     }
 }
--- WindowsFormsPrincipal/WindowsFormsPrincipal1/Program.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsPrincipal1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormPrincipal());
         }
     }
 }
