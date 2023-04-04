e6479f658fec9d2f4b552aa22ca1ea906115dd8f
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-04-04 15:40:08 -0300

    FormPrincipal
    Tela principal do sistema de gestão de hotéis

--- WindowsFormsPrincipal/WindowsFormsPrincipal1/FormPrincipal.Designer.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormPrincipal.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.SuspendLayout();
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1364, 761);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hotelogix - Gestão de Hotéis";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
