70aae45da62a3701b6e45f8a89f49e4e6c7af2d5
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-04-05 14:48:11 -0300

    correção na camada models de funcionario
    correçao de nomes

--- WindowsFormsPrincipal/Models/Funcionario.cs
+++ WindowsFormsPrincipal/Models/Funcionario.cs
@@ -11,9 +11,9 @@ namespace Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public int CPF { get; set; }
+        public string CPF { get; set; }
         public string Email { get; set; }
-        public int Celular { get; set; }
+        public string Celular { get; set; }
         public string Endereco { get; set; }
         public string NomeUsuario { get; set; }
         public int Data_nascimento { get; set; }
