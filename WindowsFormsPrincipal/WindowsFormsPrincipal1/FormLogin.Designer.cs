bee42a0f7448568fa20a8a2335a4d4b17d607ac4
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-04-05 16:57:06 -0300

    Alterações no Design do FormLogin
    Mudanças no tamanho de botões

--- WindowsFormsPrincipal/WindowsFormsPrincipal1/FormLogin.Designer.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormLogin.Designer.cs
@@ -45,8 +45,9 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 28.14545F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(582, 94);
+            this.label1.Size = new System.Drawing.Size(776, 116);
             this.label1.TabIndex = 0;
             this.label1.Text = "LOGIN";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -54,36 +55,40 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(199, 87);
+            this.pictureBox1.Location = new System.Drawing.Point(265, 107);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(188, 126);
+            this.pictureBox1.Size = new System.Drawing.Size(251, 155);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // textBoxUsuario
             // 
-            this.textBoxUsuario.Location = new System.Drawing.Point(220, 256);
+            this.textBoxUsuario.Location = new System.Drawing.Point(293, 315);
+            this.textBoxUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxUsuario.Multiline = true;
             this.textBoxUsuario.Name = "textBoxUsuario";
-            this.textBoxUsuario.Size = new System.Drawing.Size(241, 26);
+            this.textBoxUsuario.Size = new System.Drawing.Size(320, 31);
             this.textBoxUsuario.TabIndex = 2;
             // 
             // textBoxSenha
             // 
-            this.textBoxSenha.Location = new System.Drawing.Point(220, 300);
+            this.textBoxSenha.Location = new System.Drawing.Point(293, 369);
+            this.textBoxSenha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxSenha.Multiline = true;
             this.textBoxSenha.Name = "textBoxSenha";
-            this.textBoxSenha.Size = new System.Drawing.Size(241, 26);
+            this.textBoxSenha.Size = new System.Drawing.Size(320, 31);
             this.textBoxSenha.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(118, 253);
+            this.label2.Location = new System.Drawing.Point(157, 311);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 29);
+            this.label2.Size = new System.Drawing.Size(108, 31);
             this.label2.TabIndex = 3;
             this.label2.Text = "Usuário";
             // 
@@ -91,18 +96,20 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(132, 298);
+            this.label3.Location = new System.Drawing.Point(176, 367);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 29);
+            this.label3.Size = new System.Drawing.Size(92, 31);
             this.label3.TabIndex = 3;
             this.label3.Text = "Senha";
             // 
             // buttonEntrar
             // 
             this.buttonEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEntrar.Location = new System.Drawing.Point(182, 378);
+            this.buttonEntrar.Location = new System.Drawing.Point(243, 465);
+            this.buttonEntrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonEntrar.Name = "buttonEntrar";
-            this.buttonEntrar.Size = new System.Drawing.Size(107, 36);
+            this.buttonEntrar.Size = new System.Drawing.Size(143, 44);
             this.buttonEntrar.TabIndex = 4;
             this.buttonEntrar.Text = "Entrar";
             this.buttonEntrar.UseVisualStyleBackColor = true;
@@ -111,18 +118,19 @@
             // buttonCancelar
             // 
             this.buttonCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonCancelar.Location = new System.Drawing.Point(295, 378);
+            this.buttonCancelar.Location = new System.Drawing.Point(393, 465);
+            this.buttonCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonCancelar.Name = "buttonCancelar";
-            this.buttonCancelar.Size = new System.Drawing.Size(107, 36);
+            this.buttonCancelar.Size = new System.Drawing.Size(143, 44);
             this.buttonCancelar.TabIndex = 4;
             this.buttonCancelar.Text = "Cancelar";
             this.buttonCancelar.UseVisualStyleBackColor = true;
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 456);
+            this.ClientSize = new System.Drawing.Size(776, 561);
             this.Controls.Add(this.buttonCancelar);
             this.Controls.Add(this.buttonEntrar);
             this.Controls.Add(this.label3);
@@ -132,8 +140,9 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormLogin";
-            this.Text = "Login";
+            this.Text = "HOTELOGIX - Login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
