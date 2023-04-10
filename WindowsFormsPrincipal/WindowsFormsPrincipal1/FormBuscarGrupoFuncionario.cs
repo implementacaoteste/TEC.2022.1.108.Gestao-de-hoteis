f850572e1cce74a4bb4a8b11db0eeb2d308a36ec
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-04-10 16:48:11 -0300

    Adiciona Funções aos botões

--- WindowsFormsPrincipal/WindowsFormsPrincipal1/FormBuscarGrupoFuncionario.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormBuscarGrupoFuncionario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,48 @@ namespace WindowsFormsPrincipal1
         public FormBuscarGrupoFuncionario()
         {
             InitializeComponent();
+        }
+
+        private void buttonBuscarGrupoFuncionario_Click(object sender, EventArgs e)
+        {
+            grupoFuncionarioBindingSource.DataSource = new GrupoFuncionarioBLL().BuscarTodos();
+        }
+
+        private void buttonAdicionarGrupoFuncionario_Click(object sender, EventArgs e)
+        {
+            using (FormCadastroGrupoFuncionario frm = new FormCadastroGrupoFuncionario())
+            {
+                frm.ShowDialog();
+            }
+            buttonBuscarGrupoFuncionario_Click(null, null);
+        }
+
+        private void buttonAlterarGrupoFuncionario_Click(object sender, EventArgs e)
+        {
+            int id = ((GrupoFuncionario)grupoFuncionarioBindingSource.Current).Id;
+            using (FormCadastroGrupoFuncionario frm = new FormCadastroGrupoFuncionario(id))
+            {
+                frm.ShowDialog();
+            }
+            buttonBuscarGrupoFuncionario_Click(null, null);
+        }
+
+        private void buttonExcluirGrupoFuncionario_Click(object sender, EventArgs e)
+        {
+            if (grupoFuncionarioBindingSource.Count <= 0)
+            {
+                MessageBox.Show("Não existe registro para ser excluído");
+                return;
+            }
+
+            if (MessageBox.Show("Deseja realmente excluir este registro?", "Atenção", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            int id = ((GrupoFuncionario)grupoFuncionarioBindingSource.Current).Id;
+            new FuncionarioBLL().Excluir(id);
+            grupoFuncionarioBindingSource.RemoveCurrent();
+
+            MessageBox.Show("Registro excluído com sucesso!");
         }
     }
 }
