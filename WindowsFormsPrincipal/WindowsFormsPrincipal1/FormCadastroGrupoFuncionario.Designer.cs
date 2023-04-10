0f9ac1adf94bd54b7edcd97d0d75dc6acee01b5a
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-04-10 16:29:37 -0300

    Criação do FormCadastroGrupoFuncionario

---
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroGrupoFuncionario.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace WindowsFormsPrincipal1
+{
+    partial class FormCadastroGrupoFuncionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastroGrupoFuncionario));
+            this.SuspendLayout();
+            // 
+            // FormCadastroGrupoFuncionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormCadastroGrupoFuncionario";
+            this.Text = "HOTELOGIX - Cadastro de Grupo de Funcionário";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
