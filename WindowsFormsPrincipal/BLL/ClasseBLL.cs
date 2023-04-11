5e40b498879dc2135c5feb5a45308657593dde18
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-04-11 15:15:37 -0300

    Criação de metado da classe BLL
    inserir
    alterar
    buscarPortodos
    BuscarPorClasse
    excluir

--- WindowsFormsPrincipal/BLL/ClasseBLL.cs
+++ WindowsFormsPrincipal/BLL/ClasseBLL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DALL;
+using Modelss;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,28 @@ namespace BLL
 {
     public class ClasseBLL
     {
+        public void Inserir(Classe _classe)
+        {
+            new ClasseDAL().Inserir(_classe);
+        }
+
+        public void Alterar(Classe _classe)
+        {
+            new ClasseDAL().Alterar(_classe);
+        }
+
+        public List<Classe> BuscaPorTodos()
+        {
+            return new ClasseDAL().BuscaPorTodos();
+        }
+
+        public List<Classe> BuscaPorClasse(string _classe)
+        {
+            return new ClasseDAL().BuscaPorClasse(_classe);
+        }
+        public void Excluir(int _id)
+        {
+            new ClasseDAL().Excluir(_id);
+        }
     }
 }
--- WindowsFormsPrincipal/DAL/ClasseDAL.cs
+++ WindowsFormsPrincipal/DAL/ClasseDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Modelss;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace DALL
 {
     public class ClasseDAL
     {
+        public void Alterar(Classe classe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Classe> BuscaPorClasse(string classe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Classe> BuscaPorTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(Classe classe)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
