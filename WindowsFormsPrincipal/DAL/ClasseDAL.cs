8922727c9524ef7beccf0e5d194eb21d8f7577e3
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-04-11 16:25:49 -0300

    criacao de metados da classeDAL
    Alterar
    excluir
    buscarPorTodos
    BuscarPorClasse
    Inserir

--- WindowsFormsPrincipal/DAL/ClasseDAL.cs
+++ WindowsFormsPrincipal/DAL/ClasseDAL.cs
@@ -1,7 +1,10 @@
-﻿using Modelss;
+﻿using DAL;
+using Modelss;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,29 +12,157 @@ namespace DALL
 {
     public class ClasseDAL
     {
-        public void Alterar(Classe classe)
+        public void Alterar(Classe _classe)
         {
-            throw new NotImplementedException();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"UPDATE CLASSE SET CLASSE = @CLASSE 
+                                        Where ID= @ID ";
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@CLASSE", _classe.Class);
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu erro ao tentar alterar um classe no Banco de Dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
 
-        public List<Classe> BuscaPorClasse(string classe)
+        public List<Classe> BuscaPorClasse(string _classe)
         {
-            throw new NotImplementedException();
+            List<Classe> classes = new List<Classe>();
+            Classe classe;
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT ID, CLASSE
+                    FROM CLASSE Where CLASSE Like @CLASSE";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@CLASSE", "%" + _classe + "%");
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        classe = new Classe();
+                        classe.Id = Convert.ToInt32(rd["Id"]);
+                        classe.Class = rd["Nome"].ToString();
+                    }
+                }
+                return classes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar todos os usuarios: ", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
 
         public List<Classe> BuscaPorTodos()
         {
-            throw new NotImplementedException();
+            List<Classe> classes = new List<Classe>();
+            Classe classe;
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT ID, CLASSE
+                                    FROM CLASSE ";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        classe = new Classe();
+                        classe.Id = Convert.ToInt32(rd["Id"]);
+                        classe.Class = rd["Nome"].ToString();
+                       
+                    }
+                }
+                return classes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar todos as Classes: ", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
 
-        public void Excluir(int id)
+        public void Excluir(int _id)
         {
-            throw new NotImplementedException();
+
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"DELETE FROM CLASSE 
+                                    WHERE ID= @ID";
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                SqlParameter sqlParameter = cmd.Parameters.AddWithValue("@ID", _id);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu erro ao tentar excluir uma Classe no Banco de Dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
 
-        public void Inserir(Classe classe)
+        public void Inserir(Classe _classe)
         {
-            throw new NotImplementedException();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO CLASSE(CLASSE)
+                                      VALUES(@CLASSE)";
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@CLASSE", _classe.Class);
+            
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu erro ao tentar inserir uma Classe no Banco de Dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
     }
+    
 }
