ba350dd46226383457188b112e76c78f7a0d9497
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-04-11 15:50:33 -0300

    Criação dos botões e funções no FormCadastroGrupoFuncionario

--- WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroGrupoFuncionario.Designer.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroGrupoFuncionario.Designer.cs
@@ -28,21 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label nomeGrupoLabel;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastroGrupoFuncionario));
+            this.grupoFuncionarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nomeGrupoTextBox = new System.Windows.Forms.TextBox();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.labelSubTitulo = new System.Windows.Forms.Label();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            nomeGrupoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grupoFuncionarioBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // nomeGrupoLabel
+            // 
+            nomeGrupoLabel.AutoSize = true;
+            nomeGrupoLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            nomeGrupoLabel.Location = new System.Drawing.Point(22, 177);
+            nomeGrupoLabel.Name = "nomeGrupoLabel";
+            nomeGrupoLabel.Size = new System.Drawing.Size(161, 28);
+            nomeGrupoLabel.TabIndex = 0;
+            nomeGrupoLabel.Text = "Nome do grupo";
+            // 
+            // grupoFuncionarioBindingSource
+            // 
+            this.grupoFuncionarioBindingSource.DataSource = typeof(Models.GrupoFuncionario);
+            // 
+            // nomeGrupoTextBox
+            // 
+            this.nomeGrupoTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nomeGrupoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.grupoFuncionarioBindingSource, "NomeGrupo", true));
+            this.nomeGrupoTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nomeGrupoTextBox.Location = new System.Drawing.Point(27, 208);
+            this.nomeGrupoTextBox.Name = "nomeGrupoTextBox";
+            this.nomeGrupoTextBox.Size = new System.Drawing.Size(487, 34);
+            this.nomeGrupoTextBox.TabIndex = 1;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonSalvar.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSalvar.Location = new System.Drawing.Point(128, 290);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(85, 45);
+            this.buttonSalvar.TabIndex = 2;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonCancelar.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancelar.Location = new System.Drawing.Point(301, 290);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(100, 45);
+            this.buttonCancelar.TabIndex = 3;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
+            // 
+            // labelSubTitulo
+            // 
+            this.labelSubTitulo.AutoSize = true;
+            this.labelSubTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.labelSubTitulo.Font = new System.Drawing.Font("Segoe UI Black", 24F, System.Drawing.FontStyle.Bold);
+            this.labelSubTitulo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelSubTitulo.Location = new System.Drawing.Point(18, 63);
+            this.labelSubTitulo.Name = "labelSubTitulo";
+            this.labelSubTitulo.Size = new System.Drawing.Size(496, 54);
+            this.labelSubTitulo.TabIndex = 4;
+            this.labelSubTitulo.Text = "de grupo de funcionário";
+            this.labelSubTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.BackColor = System.Drawing.Color.White;
+            this.labelTitulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelTitulo.Font = new System.Drawing.Font("Segoe UI Black", 24F, System.Drawing.FontStyle.Bold);
+            this.labelTitulo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelTitulo.Location = new System.Drawing.Point(176, 9);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(196, 54);
+            this.labelTitulo.TabIndex = 4;
+            this.labelTitulo.Text = "Cadastro";
+            this.labelTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormCadastroGrupoFuncionario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(532, 403);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.labelSubTitulo);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(nomeGrupoLabel);
+            this.Controls.Add(this.nomeGrupoTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormCadastroGrupoFuncionario";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HOTELOGIX - Cadastro de Grupo de Funcionário";
+            this.Load += new System.EventHandler(this.FormCadastroGrupoFuncionario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grupoFuncionarioBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource grupoFuncionarioBindingSource;
+        private System.Windows.Forms.TextBox nomeGrupoTextBox;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.Label labelSubTitulo;
+        private System.Windows.Forms.Label labelTitulo;
     }
 }
--- WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroGrupoFuncionario.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroGrupoFuncionario.cs
@@ -1,21 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BLL;
+using Models;
+using System;
 using System.Windows.Forms;
 
 namespace WindowsFormsPrincipal1
 {
     public partial class FormCadastroGrupoFuncionario : Form
     {
-        public int id;
-        public FormCadastroGrupoFuncionario()
+        public int Id;
+        public FormCadastroGrupoFuncionario(int _id = 0)
         {
             InitializeComponent();
+            Id = _id;
+        }
+
+        private void FormCadastroGrupoFuncionario_Load(object sender, EventArgs e)
+        {
+            if (Id == 0)
+                grupoFuncionarioBindingSource.AddNew();
+            else
+            {
+                labelTitulo.Text = "Editar";
+                grupoFuncionarioBindingSource.DataSource = new GrupoFuncionarioBLL().BuscarPorId(Id);
+            }
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            GrupoFuncionarioBLL grupoFuncionarioBLL = new GrupoFuncionarioBLL();
+            grupoFuncionarioBindingSource.EndEdit();
+
+            if (Id == 0)
+                grupoFuncionarioBLL.Inserir((GrupoFuncionario)grupoFuncionarioBindingSource.Current);
+            else
+                grupoFuncionarioBLL.Alterar((GrupoFuncionario)grupoFuncionarioBindingSource.Current);
+
+            MessageBox.Show("Registro salvo com sucesso!");
+            Close();
+        }
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
