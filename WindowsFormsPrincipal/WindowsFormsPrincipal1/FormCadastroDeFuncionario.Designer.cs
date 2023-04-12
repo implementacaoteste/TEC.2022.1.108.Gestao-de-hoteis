fb8bfc26a25aa882a502a7e0703a15fa47bc2084
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-04-12 15:31:18 -0300

    Correçao de erro da tela de cadastro
    de cadastro
    cancelar
    id

--- WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroDeFuncionario.Designer.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroDeFuncionario.Designer.cs
@@ -29,15 +29,102 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label nomeLabel;
+            System.Windows.Forms.Label emailLabel;
+            System.Windows.Forms.Label cPFLabel;
+            System.Windows.Forms.Label celularLabel;
+            System.Windows.Forms.Label enderecoLabel;
+            System.Windows.Forms.Label senhaLabel;
+            System.Windows.Forms.Label ativoLabel;
+            System.Windows.Forms.Label data_nascimentoLabel;
+            System.Windows.Forms.Label nomeUsuarioLabel;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastroDeFuncionario));
             this.labelCadastroDeFuncionario = new System.Windows.Forms.Label();
             this.buttonSalvar = new System.Windows.Forms.Button();
             this.buttonCancelar = new System.Windows.Forms.Button();
             this.buttonSalvarFuncionario = new System.Windows.Forms.Button();
             this.buttonCancelarCadastro = new System.Windows.Forms.Button();
+            this.nomeTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.cPFTextBox = new System.Windows.Forms.TextBox();
+            this.celularTextBox = new System.Windows.Forms.TextBox();
+            this.enderecoTextBox = new System.Windows.Forms.TextBox();
+            this.sexosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.senhaTextBox = new System.Windows.Forms.TextBox();
+            this.ativoCheckBox = new System.Windows.Forms.CheckBox();
+            this.data_nascimentoTextBox = new System.Windows.Forms.TextBox();
+            this.nomeUsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxConfirmarSenha = new System.Windows.Forms.TextBox();
             this.funcionarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            nomeLabel = new System.Windows.Forms.Label();
+            emailLabel = new System.Windows.Forms.Label();
+            cPFLabel = new System.Windows.Forms.Label();
+            celularLabel = new System.Windows.Forms.Label();
+            enderecoLabel = new System.Windows.Forms.Label();
+            senhaLabel = new System.Windows.Forms.Label();
+            ativoLabel = new System.Windows.Forms.Label();
+            data_nascimentoLabel = new System.Windows.Forms.Label();
+            nomeUsuarioLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sexosBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.funcionarioBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // nomeLabel
+            // 
+            nomeLabel.AutoSize = true;
+            nomeLabel.Location = new System.Drawing.Point(107, 68);
+            nomeLabel.Name = "nomeLabel";
+            nomeLabel.Size = new System.Drawing.Size(44, 16);
+            nomeLabel.TabIndex = 30;
+            nomeLabel.Text = "Nome";
+            // 
+            // emailLabel
+            // 
+            emailLabel.AutoSize = true;
+            emailLabel.Location = new System.Drawing.Point(490, 68);
+            emailLabel.Name = "emailLabel";
+            emailLabel.Size = new System.Drawing.Size(45, 16);
+            emailLabel.TabIndex = 31;
+            emailLabel.Text = "E-mail";
+            // 
+            // cPFLabel
+            // 
+            cPFLabel.AutoSize = true;
+            cPFLabel.Location = new System.Drawing.Point(680, 68);
+            cPFLabel.Name = "cPFLabel";
+            cPFLabel.Size = new System.Drawing.Size(33, 16);
+            cPFLabel.TabIndex = 32;
+            cPFLabel.Text = "CPF";
+            // 
+            // celularLabel
+            // 
+            celularLabel.AutoSize = true;
+            celularLabel.Location = new System.Drawing.Point(830, 68);
+            celularLabel.Name = "celularLabel";
+            celularLabel.Size = new System.Drawing.Size(49, 16);
+            celularLabel.TabIndex = 33;
+            celularLabel.Text = "Celular";
+            // 
+            // enderecoLabel
+            // 
+            enderecoLabel.AutoSize = true;
+            enderecoLabel.Location = new System.Drawing.Point(107, 137);
+            enderecoLabel.Name = "enderecoLabel";
+            enderecoLabel.Size = new System.Drawing.Size(66, 16);
+            enderecoLabel.TabIndex = 34;
+            enderecoLabel.Text = "Endereço";
+            // 
+            // senhaLabel
+            // 
+            senhaLabel.AutoSize = true;
+            senhaLabel.Location = new System.Drawing.Point(231, 200);
+            senhaLabel.Name = "senhaLabel";
+            senhaLabel.Size = new System.Drawing.Size(46, 16);
+            senhaLabel.TabIndex = 35;
+            senhaLabel.Text = "Senha";
             // 
             // labelCadastroDeFuncionario
             // 
@@ -73,6 +160,7 @@
             this.buttonSalvarFuncionario.TabIndex = 30;
             this.buttonSalvarFuncionario.Text = "SALVAR";
             this.buttonSalvarFuncionario.UseVisualStyleBackColor = true;
+            this.buttonSalvarFuncionario.Click += new System.EventHandler(this.buttonSalvarFuncionario_Click);
             // 
             // buttonCancelarCadastro
             // 
@@ -82,16 +170,178 @@
             this.buttonCancelarCadastro.TabIndex = 30;
             this.buttonCancelarCadastro.Text = "CANCELAR";
             this.buttonCancelarCadastro.UseVisualStyleBackColor = true;
+            this.buttonCancelarCadastro.Click += new System.EventHandler(this.buttonCancelarCadastro_Click);
+            // 
+            // nomeTextBox
+            // 
+            this.nomeTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Nome", true));
+            this.nomeTextBox.Location = new System.Drawing.Point(110, 87);
+            this.nomeTextBox.Name = "nomeTextBox";
+            this.nomeTextBox.Size = new System.Drawing.Size(361, 22);
+            this.nomeTextBox.TabIndex = 31;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Email", true));
+            this.emailTextBox.Location = new System.Drawing.Point(493, 87);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(184, 22);
+            this.emailTextBox.TabIndex = 32;
+            // 
+            // cPFTextBox
+            // 
+            this.cPFTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "CPF", true));
+            this.cPFTextBox.Location = new System.Drawing.Point(683, 87);
+            this.cPFTextBox.Name = "cPFTextBox";
+            this.cPFTextBox.Size = new System.Drawing.Size(123, 22);
+            this.cPFTextBox.TabIndex = 33;
+            // 
+            // celularTextBox
+            // 
+            this.celularTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Celular", true));
+            this.celularTextBox.Location = new System.Drawing.Point(833, 87);
+            this.celularTextBox.Name = "celularTextBox";
+            this.celularTextBox.Size = new System.Drawing.Size(144, 22);
+            this.celularTextBox.TabIndex = 34;
+            // 
+            // enderecoTextBox
+            // 
+            this.enderecoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Endereco", true));
+            this.enderecoTextBox.Location = new System.Drawing.Point(110, 156);
+            this.enderecoTextBox.Name = "enderecoTextBox";
+            this.enderecoTextBox.Size = new System.Drawing.Size(361, 22);
+            this.enderecoTextBox.TabIndex = 35;
+            // 
+            // sexosBindingSource
+            // 
+            this.sexosBindingSource.DataMember = "Sexos";
+            this.sexosBindingSource.DataSource = this.funcionarioBindingSource;
+            // 
+            // senhaTextBox
+            // 
+            this.senhaTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Senha", true));
+            this.senhaTextBox.Location = new System.Drawing.Point(234, 219);
+            this.senhaTextBox.Name = "senhaTextBox";
+            this.senhaTextBox.Size = new System.Drawing.Size(152, 22);
+            this.senhaTextBox.TabIndex = 36;
+            // 
+            // ativoLabel
+            // 
+            ativoLabel.AutoSize = true;
+            ativoLabel.Location = new System.Drawing.Point(885, 162);
+            ativoLabel.Name = "ativoLabel";
+            ativoLabel.Size = new System.Drawing.Size(40, 16);
+            ativoLabel.TabIndex = 36;
+            ativoLabel.Text = "Ativo:";
+            // 
+            // ativoCheckBox
+            // 
+            this.ativoCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("CheckState", this.funcionarioBindingSource, "Ativo", true));
+            this.ativoCheckBox.Location = new System.Drawing.Point(931, 158);
+            this.ativoCheckBox.Name = "ativoCheckBox";
+            this.ativoCheckBox.Size = new System.Drawing.Size(104, 24);
+            this.ativoCheckBox.TabIndex = 37;
+            this.ativoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // data_nascimentoLabel
+            // 
+            data_nascimentoLabel.AutoSize = true;
+            data_nascimentoLabel.Location = new System.Drawing.Point(680, 139);
+            data_nascimentoLabel.Name = "data_nascimentoLabel";
+            data_nascimentoLabel.Size = new System.Drawing.Size(108, 16);
+            data_nascimentoLabel.TabIndex = 37;
+            data_nascimentoLabel.Text = "Data nascimento";
+            // 
+            // data_nascimentoTextBox
+            // 
+            this.data_nascimentoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Data_nascimento", true));
+            this.data_nascimentoTextBox.Location = new System.Drawing.Point(683, 158);
+            this.data_nascimentoTextBox.Name = "data_nascimentoTextBox";
+            this.data_nascimentoTextBox.Size = new System.Drawing.Size(176, 22);
+            this.data_nascimentoTextBox.TabIndex = 38;
+            // 
+            // nomeUsuarioLabel
+            // 
+            nomeUsuarioLabel.AutoSize = true;
+            nomeUsuarioLabel.Location = new System.Drawing.Point(107, 200);
+            nomeUsuarioLabel.Name = "nomeUsuarioLabel";
+            nomeUsuarioLabel.Size = new System.Drawing.Size(94, 16);
+            nomeUsuarioLabel.TabIndex = 38;
+            nomeUsuarioLabel.Text = "Nome Usuario";
+            // 
+            // nomeUsuarioTextBox
+            // 
+            this.nomeUsuarioTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "NomeUsuario", true));
+            this.nomeUsuarioTextBox.Location = new System.Drawing.Point(110, 219);
+            this.nomeUsuarioTextBox.Name = "nomeUsuarioTextBox";
+            this.nomeUsuarioTextBox.Size = new System.Drawing.Size(100, 22);
+            this.nomeUsuarioTextBox.TabIndex = 39;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.sexosBindingSource, "Genero", true));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(493, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 40;
+            // 
+            // textBoxConfirmarSenha
+            // 
+            this.textBoxConfirmarSenha.Location = new System.Drawing.Point(419, 219);
+            this.textBoxConfirmarSenha.Name = "textBoxConfirmarSenha";
+            this.textBoxConfirmarSenha.Size = new System.Drawing.Size(169, 22);
+            this.textBoxConfirmarSenha.TabIndex = 41;
             // 
             // funcionarioBindingSource
             // 
             this.funcionarioBindingSource.DataSource = typeof(Models.Funcionario);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Confirmar Senha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(493, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Genero";
+            // 
             // FormCadastroDeFuncionario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1122, 420);
+            this.ClientSize = new System.Drawing.Size(1122, 417);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxConfirmarSenha);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(nomeUsuarioLabel);
+            this.Controls.Add(this.nomeUsuarioTextBox);
+            this.Controls.Add(data_nascimentoLabel);
+            this.Controls.Add(this.data_nascimentoTextBox);
+            this.Controls.Add(ativoLabel);
+            this.Controls.Add(this.ativoCheckBox);
+            this.Controls.Add(senhaLabel);
+            this.Controls.Add(this.senhaTextBox);
+            this.Controls.Add(enderecoLabel);
+            this.Controls.Add(this.enderecoTextBox);
+            this.Controls.Add(celularLabel);
+            this.Controls.Add(this.celularTextBox);
+            this.Controls.Add(cPFLabel);
+            this.Controls.Add(this.cPFTextBox);
+            this.Controls.Add(emailLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(nomeLabel);
+            this.Controls.Add(this.nomeTextBox);
             this.Controls.Add(this.buttonCancelarCadastro);
             this.Controls.Add(this.buttonSalvarFuncionario);
             this.Controls.Add(this.labelCadastroDeFuncionario);
@@ -100,8 +350,11 @@
             this.MinimizeBox = false;
             this.Name = "FormCadastroDeFuncionario";
             this.Text = "HOTELOGIX - Cadastro de Funcionários";
+            this.Load += new System.EventHandler(this.FormCadastroDeFuncionario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sexosBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.funcionarioBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -112,5 +365,19 @@
         private System.Windows.Forms.Button buttonSalvarFuncionario;
         private System.Windows.Forms.Button buttonCancelarCadastro;
         private System.Windows.Forms.BindingSource funcionarioBindingSource;
+        private System.Windows.Forms.TextBox nomeTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox cPFTextBox;
+        private System.Windows.Forms.TextBox celularTextBox;
+        private System.Windows.Forms.TextBox enderecoTextBox;
+        private System.Windows.Forms.BindingSource sexosBindingSource;
+        private System.Windows.Forms.TextBox senhaTextBox;
+        private System.Windows.Forms.CheckBox ativoCheckBox;
+        private System.Windows.Forms.TextBox data_nascimentoTextBox;
+        private System.Windows.Forms.TextBox nomeUsuarioTextBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxConfirmarSenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroDeFuncionario.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroDeFuncionario.cs
@@ -1,4 +1,5 @@
 ﻿using BLL;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,47 +12,44 @@ using System.Windows.Forms;
 
 namespace WindowsFormsPrincipal1
 {
+   
     public partial class FormCadastroDeFuncionario : Form
     {
-        public FormCadastroDeFuncionario()
+        public int Id;
+        public FormCadastroDeFuncionario(int _id = 0)
         {
             InitializeComponent();
-        }
-
-        private void FormCadastroDeFuncionario_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBoxAtivo_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonCancelar_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
-
-        private void buttonSalvar_Click(object sender, EventArgs e)
-        {
-            
+            Id = _id;
         }
 
         private void buttonSalvarFuncionario_Click(object sender, EventArgs e)
         {
-            FuncionarioBLL funcionarioBLL = new FuncionarioBLL();
-            
+            try
+            {
+                FuncionarioBLL funcionarioBLL = new FuncionarioBLL();
+                funcionarioBindingSource.EndEdit();
+                if (Id == 0)
+                    funcionarioBLL.Inserir((Funcionario)funcionarioBindingSource.Current, textBoxConfirmarSenha.Text);
+                else
+                    funcionarioBLL.Alterar((Funcionario)funcionarioBindingSource.Current, textBoxConfirmarSenha.Text);
+            }
+            catch
+            {
+
+            }
         }
 
         private void buttonCancelarCadastro_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void FormCadastroDeFuncionario_Load(object sender, EventArgs e)
+        {
+            if (Id == 0)
+                funcionarioBindingSource.AddNew();
+            else
+                funcionarioBindingSource.DataSource = new FuncionarioBLL().BuscarPorId(Id);
         }
     }
 }
