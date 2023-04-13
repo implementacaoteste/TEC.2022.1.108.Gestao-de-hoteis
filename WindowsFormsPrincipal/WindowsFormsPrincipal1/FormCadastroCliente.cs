7d1c74dcf385db6633c85122f0f04f3524830b2a
Alana <115494193+alana1789@users.noreply.github.com>
2023-04-13 15:34:20 -0300

    ButtonSalvar

--- WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroCliente.cs
+++ WindowsFormsPrincipal/WindowsFormsPrincipal1/FormCadastroCliente.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BLL;
+using Models;
+using Modelss;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,36 @@ namespace WindowsFormsPrincipal1
         public FormCadastroCliente()
         {
             InitializeComponent();
+        }
+
+        public int Id;
+        public FormCadastroCliente(int _id = 0)
+        {
+            InitializeComponent();
+            Id = _id;
+        }
+
+        private void buttonCancelarCadastro_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonSalvarFuncionario_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClienteBLL clienteBLL = new ClienteBLL();
+                ClientebindingSource.EndEdit();
+                if (Id == 0)
+                    clienteBLL.Inserir((Cliente)ClientebindingSource.Current);
+                else
+                    clienteBLL.Alterar((Cliente)ClientebindingSource.Current);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Ocorreu erro ao tentar salvar um cliente no Banco de Dados", ex);
+            }
+
         }
     }
 }
