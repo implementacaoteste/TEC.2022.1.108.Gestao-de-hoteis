e9e9cff3a55c62ad31de0f3bf607f37432cb9ceb
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-04-20 15:26:51 -0300

    formBuscarDiaria Desiger2.0

--- WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormBuscarDiaria.Designer.cs
+++ WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormBuscarDiaria.Designer.cs
@@ -29,11 +29,194 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormBuscarDiaria));
+            this.radioButtonDT_EN = new System.Windows.Forms.RadioButton();
+            this.radioButtonNome = new System.Windows.Forms.RadioButton();
+            this.radioButtonCPF = new System.Windows.Forms.RadioButton();
+            this.textBoxDt_En = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.diariaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.diariaDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.diariaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diariaDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButtonDT_EN
+            // 
+            this.radioButtonDT_EN.AutoSize = true;
+            this.radioButtonDT_EN.Location = new System.Drawing.Point(43, 12);
+            this.radioButtonDT_EN.Name = "radioButtonDT_EN";
+            this.radioButtonDT_EN.Size = new System.Drawing.Size(126, 20);
+            this.radioButtonDT_EN.TabIndex = 0;
+            this.radioButtonDT_EN.TabStop = true;
+            this.radioButtonDT_EN.Text = "Data da Entrada";
+            this.radioButtonDT_EN.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNome
+            // 
+            this.radioButtonNome.AutoSize = true;
+            this.radioButtonNome.Location = new System.Drawing.Point(43, 88);
+            this.radioButtonNome.Name = "radioButtonNome";
+            this.radioButtonNome.Size = new System.Drawing.Size(65, 20);
+            this.radioButtonNome.TabIndex = 0;
+            this.radioButtonNome.TabStop = true;
+            this.radioButtonNome.Text = "Nome";
+            this.radioButtonNome.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCPF
+            // 
+            this.radioButtonCPF.AutoSize = true;
+            this.radioButtonCPF.Location = new System.Drawing.Point(166, 88);
+            this.radioButtonCPF.Name = "radioButtonCPF";
+            this.radioButtonCPF.Size = new System.Drawing.Size(54, 20);
+            this.radioButtonCPF.TabIndex = 0;
+            this.radioButtonCPF.TabStop = true;
+            this.radioButtonCPF.Text = "CPF";
+            this.radioButtonCPF.UseVisualStyleBackColor = true;
+            // 
+            // textBoxDt_En
+            // 
+            this.textBoxDt_En.Location = new System.Drawing.Point(43, 51);
+            this.textBoxDt_En.Name = "textBoxDt_En";
+            this.textBoxDt_En.Size = new System.Drawing.Size(226, 22);
+            this.textBoxDt_En.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(226, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(285, 128);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(75, 23);
+            this.buttonBuscar.TabIndex = 3;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            // 
+            // diariaBindingSource
+            // 
+            this.diariaBindingSource.DataSource = typeof(Models.Diaria);
+            // 
+            // diariaDataGridView
+            // 
+            this.diariaDataGridView.AllowUserToAddRows = false;
+            this.diariaDataGridView.AllowUserToDeleteRows = false;
+            this.diariaDataGridView.AllowUserToOrderColumns = true;
+            this.diariaDataGridView.AutoGenerateColumns = false;
+            this.diariaDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.diariaDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6});
+            this.diariaDataGridView.DataSource = this.diariaBindingSource;
+            this.diariaDataGridView.Location = new System.Drawing.Point(12, 176);
+            this.diariaDataGridView.Name = "diariaDataGridView";
+            this.diariaDataGridView.ReadOnly = true;
+            this.diariaDataGridView.RowHeadersWidth = 51;
+            this.diariaDataGridView.RowTemplate.Height = 24;
+            this.diariaDataGridView.Size = new System.Drawing.Size(568, 220);
+            this.diariaDataGridView.TabIndex = 7;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.FillWeight = 35F;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Cód.";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Id_cliente";
+            this.dataGridViewTextBoxColumn2.FillWeight = 40F;
+            this.dataGridViewTextBoxColumn2.HeaderText = "Cliente";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Data_Entrada";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Data da Entrada";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Id_funcionario";
+            this.dataGridViewTextBoxColumn5.FillWeight = 50F;
+            this.dataGridViewTextBoxColumn5.HeaderText = "Funcionário";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            this.dataGridViewTextBoxColumn5.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "Data_Saida";
+            this.dataGridViewTextBoxColumn6.FillWeight = 80F;
+            this.dataGridViewTextBoxColumn6.HeaderText = "Data da Saida";
+            this.dataGridViewTextBoxColumn6.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
+            // FormBuscarDiaria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormBuscarDiaria";
+            this.ClientSize = new System.Drawing.Size(1040, 570);
+            this.Controls.Add(this.diariaDataGridView);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxDt_En);
+            this.Controls.Add(this.radioButtonCPF);
+            this.Controls.Add(this.radioButtonNome);
+            this.Controls.Add(this.radioButtonDT_EN);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormBuscarDiaria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form Buscar de Diária";
+            ((System.ComponentModel.ISupportInitialize)(this.diariaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diariaDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonDT_EN;
+        private System.Windows.Forms.RadioButton radioButtonNome;
+        private System.Windows.Forms.RadioButton radioButtonCPF;
+        private System.Windows.Forms.TextBox textBoxDt_En;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.BindingSource diariaBindingSource;
+        private System.Windows.Forms.DataGridView diariaDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
     }
 }
