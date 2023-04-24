c7cd3480031afb79c47ab07c7ed632a5141139e3
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-04-24 16:32:25 -0300

    PagamentoBLL metados
    alterar
    excluir
    inserir
    buscarportodos
    buscarPorid

--- WindowsFormsPrincipal/DAL/PagamentoDAL.cs
+++ WindowsFormsPrincipal/DAL/PagamentoDAL.cs
@@ -1,7 +1,36 @@
 ﻿
+using Models;
+using System;
+using System.Collections.Generic;
+
 namespace DAL
 {
     public class PagamentoDAL
     {
+        public void Alterar(Pagamento pagamento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pagamento> BuscaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pagamento> BuscaPorTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(Pagamento pagamento)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
