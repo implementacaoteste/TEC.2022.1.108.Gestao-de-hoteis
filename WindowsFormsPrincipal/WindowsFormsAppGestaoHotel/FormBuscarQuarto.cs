56bd8fc80fd118ba4d4f1019de5efafab76a005c
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-04-26 17:24:18 -0300

    FormBuscarQuarto

--- WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormBuscarQuarto.cs
+++ WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormBuscarQuarto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,42 @@ namespace WindowsFormsAppGestaoHotel
 {
     public partial class FormBuscarQuarto : Form
     {
-        public FormBuscarQuarto()
+        public int Numero;
+        public FormBuscarQuarto(int _numero = 0)
         {
             InitializeComponent();
+            Numero = _numero;
+        }
+        private void FormBuscarQuarto_Load(object sender, EventArgs e)
+        {
+            radioButtonTodos.Checked = true;
+            quartoBindingSource.DataSource = new QuartoBLL().BuscarPorTodos();
         }
 
         private void buttonBuscarQuarto_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (radioButtonTodos.Checked)
+                {
+                    textBoxBuscar.Clear();
+                    quartoBindingSource.DataSource = new QuartoBLL().BuscarPorTodos();
+                }
+                else if (radioButtonNumero.Checked)
+                {
+                    quartoBindingSource.DataSource = new QuartoBLL().BuscarPorNumero(Numero);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void valor_DiariaTextBox_TextChanged(object sender, EventArgs e)
+        {
 
         }
+
     }
 }
