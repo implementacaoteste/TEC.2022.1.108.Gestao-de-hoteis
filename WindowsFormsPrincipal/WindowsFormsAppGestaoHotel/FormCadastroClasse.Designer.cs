bbdc7ecd72fdb04c257e1bc0ab201130c3c5ce9b
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-04-27 17:17:22 -0300

    Alterações

--- WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormCadastroClasse.Designer.cs
+++ WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormCadastroClasse.Designer.cs
@@ -117,7 +117,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(452, 245);
+            this.ClientSize = new System.Drawing.Size(452, 248);
             this.Controls.Add(this.buttonCancelar);
             this.Controls.Add(this.buttonSalvar);
             this.Controls.Add(classLabel);
