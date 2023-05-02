7924423e7c9ce1abf3ec88977d330416230f5252
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-05-02 14:57:18 -0300

    Correções

--- WindowsFormsPrincipal/Models/Diaria.cs
+++ WindowsFormsPrincipal/Models/Diaria.cs
@@ -9,14 +9,14 @@ namespace Models
     public class Diaria
     {
         public int Id { get; set; }
-        public int Id_cliente { get; set; }
-        public string Nome_Cliente { get; set; }
-        public string Cpf_Cliente { get; set; }
-        public float Valor_Total { get; set; }
-        public string Data_Entrada { get; set; }
-        public int Id_funcionario { get; set; }
-        public string Data_Saida { get; set; }
+        public int Id_Cliente { get; set; }
+        public int Id_Funcionario { get; set; }
         public int Id_Pagamento { get; set; }
+        public string Nome_Cliente { get; set; }
+        public string CPF_Cliente { get; set; }
+        public float Valor_Total { get; set; }
+        public DateTime Data_Entrada { get; set; }
+        public DateTime Data_Saida { get; set; }
         public List<Quarto> Quartos { get; set; }
     }
 }
