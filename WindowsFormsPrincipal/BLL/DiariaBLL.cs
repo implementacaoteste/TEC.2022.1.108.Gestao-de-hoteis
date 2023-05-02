279bcf9826cc2e3b1478235c81d2186c4f960f92
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-05-02 17:19:01 -0300

    Correções e implementação de métodos

--- WindowsFormsPrincipal/BLL/DiariaBLL.cs
+++ WindowsFormsPrincipal/BLL/DiariaBLL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using DAL;
 using Models;
 
@@ -9,38 +10,48 @@ namespace BLL
     {
         public void Inserir(Diaria _diaria)
         {
+            ValidarPermissao(14);
             new DiariaDAL().Inserir(_diaria);
         }
         public void Alterar(Diaria _diaria)
         {
+            ValidarPermissao(15);
             new DiariaDAL().Alterar(_diaria);
         }
         public void Excluir(int _id)
         {
+            ValidarPermissao(16);
             new DiariaDAL().Excluir(_id);
+        }
+        public Diaria BuscarPorId(int _id)
+        {
+            ValidarPermissao(13);
+            return new DiariaDAL().BuscarPorId(_id);
         }
         public List<Diaria> BuscarPorTodos()
         {
+            ValidarPermissao(13);
             return new DiariaDAL().BuscarPorTodos();
-        }
-        public void ValidarPermissao(int _idPermissao)
-        {
-            if (!new GrupoFuncionarioDAL().ValidarPermissao(Constante.IdLogado, _idPermissao))
-                throw new Exception("Não foi possível realizar essa operação.");
         }
         public List<Diaria> BuscarPorCPFCliente(string _cpf)
         {
+            ValidarPermissao(13);
             return new DiariaDAL().BuscarPorCPFCliente(_cpf);
         }
-
         public List<Diaria> BuscarPorNomeCliente(string _nome)
         {
+            ValidarPermissao(13);
             return new DiariaDAL().BuscarPorNomeCliente(_nome);
         }
-
         public List<Diaria> BuscarPorDataEntrada(string _dataEntrada)
         {
+            ValidarPermissao(13);
             return new DiariaDAL().BuscarPorDataEntrada(_dataEntrada);
+        }
+        public void ValidarPermissao(int _idPermissao)
+        {
+            if (!new DiariaDAL().ValidarPermissao(Constante.IdLogado, _idPermissao))
+                throw new Exception("Você não tem permissão de realizar essa operação. Procure o administrador do sistema.");
         }
     }
 }
--- WindowsFormsPrincipal/DAL/DiariaDAL.cs
+++ WindowsFormsPrincipal/DAL/DiariaDAL.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,16 +18,15 @@ namespace DAL
             try
             {
                 SqlCommand cmd = cn.CreateCommand();
-                cmd.CommandText = @"INSET INTO (VALOR_TOTAL, DATA_ENTRADA, ID_CLIENTE, ID_FUNCIONARIO, ID_PAGAMENTO, DATA_SAIDA, DATA_ENRADA)
-                                       VALUES(@VALOR_TOTAL, @DATA_ENTRADA, @ID_CLIENTE, @ID_FUNCIONARIO, @ID_PAGAMENTO, @DATA_SAIDA, @DATA_ENTRADA)";
+                cmd.CommandText = @"INSERT INTO DIARIA (VALOR_TOTAL, ID_CLIENTE, ID_FUNCIONARIO, ID_PAGAMENTO, DATA_ENTRADA, DATA_SAIDA)
+                                       VALUES(@VALOR_TOTAL, @ID_CLIENTE, @ID_FUNCIONARIO, @ID_PAGAMENTO, @DATA_ENTRADA, @DATA_SAIDA)";
                 cmd.CommandType = System.Data.CommandType.Text;
                 cmd.Parameters.AddWithValue("@VALOR_TOTAL", _diaria.Valor_Total);
-                cmd.Parameters.AddWithValue("DATA_ENTRADA", _diaria.Data_Entrada);
-                cmd.Parameters.AddWithValue("@ID_CLIENTE", _diaria.Id_cliente);
-                cmd.Parameters.AddWithValue("@ID_FUNCIONARIO", _diaria.Id_funcionario);
+                cmd.Parameters.AddWithValue("@ID_CLIENTE", _diaria.Id_Cliente);
+                cmd.Parameters.AddWithValue("@ID_FUNCIONARIO", _diaria.Id_Funcionario);
                 cmd.Parameters.AddWithValue("@ID_PAGAMENTO", _diaria.Id_Pagamento);
+                cmd.Parameters.AddWithValue("DATA_ENTRADA", _diaria.Data_Entrada);
                 cmd.Parameters.AddWithValue("@DATA_SAIDA", _diaria.Data_Saida);
-                cmd.Parameters.AddWithValue("@DATA_ENTRADA", _diaria.Data_Entrada);
                 cmd.Connection = cn;
                 cn.Open();
 
@@ -34,7 +34,7 @@ namespace DAL
             }
             catch (Exception ex)
             {
-                throw new Exception("Ocorreu um erro ao tentar inserir a diaria no Banco de Dados.", ex);
+                throw new Exception("Ocorreu um erro ao tentar inserir a Diária no Banco de Dados.", ex);
             }
             finally
             {
@@ -47,16 +47,18 @@ namespace DAL
             try
             {
                 SqlCommand cmd = cn.CreateCommand();
-                cmd.CommandText = @"UPDATE DIARIA SET VALOR_TOTAL = @VALOR_TOTAL, DATA_ENTRADA = @DATA_ENTRADA, ID_CLIENTE = @ID_CLIENTE, ID_FUNCIONARIO = @ID_FUNCIONARIO,ID_PAGAMENTO = @ID_PAGAMENTO, DATA_SAIDA = @DATA_SAIDA, DATA_ENRADA = @DATA_ENRADA
+                cmd.CommandText = @"UPDATE DIARIA SET VALOR_TOTAL = @VALOR_TOTAL, ID_CLIENTE = @ID_CLIENTE, 
+                                    ID_FUNCIONARIO = @ID_FUNCIONARIO, ID_PAGAMENTO = @ID_PAGAMENTO, 
+                                    DATA_ENTRADA = @DATA_ENTRADA, DATA_SAIDA = @DATA_SAIDA
                                     WHERE ID = @ID";
-
                 cmd.CommandType = System.Data.CommandType.Text;
                 cmd.Parameters.AddWithValue("@VALOR_TOTAL", _diaria.Valor_Total);
-                cmd.Parameters.AddWithValue("DATA_ENTRADA", _diaria.Data_Entrada);
-                cmd.Parameters.AddWithValue("@ID_CLIENTE", _diaria.Id_cliente);
-                cmd.Parameters.AddWithValue("@ID_FUNCIONARIO", _diaria.Id_funcionario);
+                cmd.Parameters.AddWithValue("@ID_CLIENTE", _diaria.Id_Cliente);
+                cmd.Parameters.AddWithValue("@ID_FUNCIONARIO", _diaria.Id_Funcionario);
                 cmd.Parameters.AddWithValue("@ID_PAGAMENTO", _diaria.Id_Pagamento);
+                cmd.Parameters.AddWithValue("DATA_ENTRADA", _diaria.Data_Entrada);
                 cmd.Parameters.AddWithValue("@DATA_SAIDA", _diaria.Data_Saida);
+                cmd.Parameters.AddWithValue("@ID", _diaria.Id);
                 cmd.Connection = cn;
                 cn.Open();
 
@@ -64,7 +66,7 @@ namespace DAL
             }
             catch (Exception ex)
             {
-                throw new Exception("Ocorreu um erro ao tentar alterar a diaria no Banco de Dados.", ex);
+                throw new Exception("Ocorreu um erro ao tentar alterar a Diária no Banco de Dados.", ex);
             }
             finally
             {
@@ -89,7 +91,50 @@ namespace DAL
             }
             catch (Exception ex)
             {
-                throw new Exception("Ocorreu erro ao tentar excluir uma diaria no Banco de Dados.", ex);
+                throw new Exception("Ocorreu erro ao tentar excluir uma Diária no Banco de Dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public Diaria BuscarPorId(int _id)
+        {
+            Diaria diaria = new Diaria();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT DIARIA.ID, VALOR_TOTAL, DATA_ENTRADA, ID_CLIENTE, ID_FUNCIONARIO, ID_PAGAMENTO, DATA_SAIDA
+                                    FROM DIARIA 
+                                    INNER JOIN CLIENTE ON DIARIA.ID_CLIENTE = CLIENTE.ID
+                                    INNER JOIN FUNCIONARIO ON DIARIA.ID_FUNCIONARIO = FUNCIONARIO.ID
+                                    INNER JOIN PAGAMENTO ON DIARIA.ID_PAGAMENTO = PAGAMENTO.ID
+                                    WHERE DIARIA.ID=@ID";
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@ID", _id);
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        diaria.Id = Convert.ToInt32(rd["ID"]);
+                        diaria.Valor_Total = (double)rd["VALOR_TOTAL"];
+                        diaria.Data_Entrada = Convert.ToDateTime(rd["DATA_ENTRADA"]);
+                        diaria.Id_Cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
+                        diaria.Id_Funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
+                        diaria.Id_Pagamento = Convert.ToInt32(rd["ID_PAGAMENTO"]);
+                        diaria.Data_Saida = Convert.ToDateTime(rd["DATA_SAIDA"]);
+                    }
+                }
+                return diaria;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Ocorreu um erro ao tentar buscar a Diária na buscar por ID.", ex);
             }
             finally
             {
@@ -99,14 +144,15 @@ namespace DAL
         public List<Diaria> BuscarPorTodos()
         {
             SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
-            List<Diaria> Diaria = new List<Diaria>();
+            List<Diaria> diarias = new List<Diaria>();
             Diaria diaria;
             try
             {
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = cn;
-                cmd.CommandText = @"SELECT ID, VALOR_TOTAL, DATA_ENTRADA, ID_CLIENTE, ID_FUNCIONARIO, ID_PAGAMENTO, DATA_SAIDA, DATA_ENTRADA
-                                    FROM Diaria";
+                cmd.CommandText = @"SELECT ID, VALOR_TOTAL, ID_CLIENTE, ID_FUNCIONARIO, 
+                                    ID_PAGAMENTO, DATA_ENTRADA, DATA_SAIDA
+                                    FROM DIARIA";
                 cmd.CommandType = System.Data.CommandType.Text;
                 cn.Open();
 
@@ -116,29 +162,27 @@ namespace DAL
                     {
                         diaria = new Diaria();
                         diaria.Id = Convert.ToInt32(rd["ID"]);
-                        diaria.Id_funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
-                        diaria.Id_cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
-                        diaria.Id_Pagamento = Convert.ToInt32(rd["ID_pagamento"]);
-                        diaria.Valor_Total = Convert.ToSingle(rd["VALOR_TOTAL"]);
-                        diaria.Data_Entrada = rd["DATA_ENTRADA"].ToString();
-                        diaria.Data_Saida = rd["DATA_SAIDA"].ToString();
-                        Diaria.Add(diaria);
+                        diaria.Id_Funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
+                        diaria.Id_Cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
+                        diaria.Id_Pagamento = Convert.ToInt32(rd["ID_PAGAMENTO"]);
+                        diaria.Valor_Total = (double)rd["VALOR_TOTAL"];
+                        diaria.Data_Entrada = Convert.ToDateTime(rd["DATA_ENTRADA"]);
+                        diaria.Data_Saida = Convert.ToDateTime(rd["DATA_SAIDA"]);
+                        diaria.Quartos = new QuartoDAL().BuscarPorIdDiaria(diaria.Id);
+                        diarias.Add(diaria);
                     }
-
                 }
-                return Diaria;
+                return diarias;
             }
             catch (Exception ex)
             {
-                throw new Exception("Ocorreu um erro ao tentar buscar todas as diarias");
+                throw new Exception("Ocorreu um erro ao tentar buscar todas as Diárias.", ex);
             }
             finally
             {
                 cn.Close();
             }
-
         }
-
         public List<Diaria> BuscarPorCPFCliente(string _cpf)
         {
             SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
@@ -163,14 +207,14 @@ namespace DAL
                     {
                         diaria = new Diaria();
                         diaria.Id = Convert.ToInt32(rd["ID"]);
-                        diaria.Id_funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
+                        diaria.Id_Funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
                         diaria.Nome_Cliente = rd["NOME"].ToString();
-                        diaria.Cpf_Cliente = rd["CPF"].ToString();
-                        diaria.Id_cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
+                        diaria.CPF_Cliente = rd["CPF"].ToString();
+                        diaria.Id_Cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
                         diaria.Id_Pagamento = Convert.ToInt32(rd["ID_pagamento"]);
                         diaria.Valor_Total = Convert.ToSingle(rd["VALOR_TOTAL"]);
-                        diaria.Data_Entrada = rd["DATA_ENTRADA"].ToString();
-                        diaria.Data_Saida = rd["DATA_SAIDA"].ToString();
+                        diaria.Data_Entrada = Convert.ToDateTime(rd["DATA_ENTRADA"]);
+                        diaria.Data_Saida = Convert.ToDateTime(rd["DATA_SAIDA"]);
                         Diaria.Add(diaria);
                     }
 
@@ -186,7 +230,6 @@ namespace DAL
                 cn.Close();
             }
         }
-
         public List<Diaria> BuscarPorNomeCliente(string _nome)
         {
             SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
@@ -211,14 +254,14 @@ namespace DAL
                     {
                         diaria = new Diaria();
                         diaria.Id = Convert.ToInt32(rd["ID"]);
-                        diaria.Id_funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
+                        diaria.Id_Funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
                         diaria.Nome_Cliente = rd["NOME"].ToString();
-                        diaria.Cpf_Cliente = rd["CPF"].ToString();
-                        diaria.Id_cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
+                        diaria.CPF_Cliente = rd["CPF"].ToString();
+                        diaria.Id_Cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
                         diaria.Id_Pagamento = Convert.ToInt32(rd["ID_pagamento"]);
                         diaria.Valor_Total = Convert.ToSingle(rd["VALOR_TOTAL"]);
-                        diaria.Data_Entrada = rd["DATA_ENTRADA"].ToString();
-                        diaria.Data_Saida = rd["DATA_SAIDA"].ToString();
+                        diaria.Data_Entrada = Convert.ToDateTime(rd["DATA_ENTRADA"]);
+                        diaria.Data_Saida = Convert.ToDateTime(rd["DATA_SAIDA"]);
                         Diaria.Add(diaria);
                     }
 
@@ -234,7 +277,6 @@ namespace DAL
                 cn.Close();
             }
         }
-
         public List<Diaria> BuscarPorDataEntrada(string _dataEntrada)
         {
             SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
@@ -259,14 +301,14 @@ namespace DAL
                     {
                         diaria = new Diaria();
                         diaria.Id = Convert.ToInt32(rd["ID"]);
-                        diaria.Id_funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
+                        diaria.Id_Funcionario = Convert.ToInt32(rd["ID_FUNCIONARIO"]);
                         diaria.Nome_Cliente = rd["NOME"].ToString();
-                        diaria.Cpf_Cliente = rd["CPF"].ToString();
-                        diaria.Id_cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
-                        diaria.Id_Pagamento = Convert.ToInt32(rd["ID_pagamento"]);
-                        diaria.Valor_Total = Convert.ToSingle(rd["VALOR_TOTAL"]);
-                        diaria.Data_Entrada = rd["DATA_ENTRADA"].ToString();
-                        diaria.Data_Saida = rd["DATA_SAIDA"].ToString();
+                        diaria.CPF_Cliente = rd["CPF"].ToString();
+                        diaria.Id_Cliente = Convert.ToInt32(rd["ID_CLIENTE"]);
+                        diaria.Id_Pagamento = Convert.ToInt32(rd["ID_PAGAMENTO"]);
+                        diaria.Valor_Total = (double)rd["VALOR_TOTAL"];
+                        diaria.Data_Entrada = Convert.ToDateTime(rd["DATA_ENTRADA"]);
+                        diaria.Data_Saida = Convert.ToDateTime(rd["DATA_SAIDA"]);
                         Diaria.Add(diaria);
                     }
 
@@ -275,7 +317,38 @@ namespace DAL
             }
             catch (Exception ex)
             {
-                throw new Exception("Ocorreu um erro ao tentar buscar a data de entrada no banco de dados", ex);
+                throw new Exception("Ocorreu um erro ao tentar buscar a data de entrada no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public bool ValidarPermissao(int _idFuncionario, int _idPermissao)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT 1 FROM PERMISSAO_GRUPO_FUNCIONARIO
+                                    INNER JOIN FUNCIONARIO_GRUPO_FUNCIONARIO ON PERMISSAO_GRUPO_FUNCIONARIO.ID_GRUPO_FUNCIONARIO = FUNCIONARIO_GRUPO_FUNCIONARIO.ID_GRUPO_FUNCIONARIO
+                                    WHERE FUNCIONARIO_GRUPO_FUNCIONARIO.ID_FUNCIONARIO = @IdFuncionario AND PERMISSAO_GRUPO_FUNCIONARIO.ID_PERMISSAO = @IdPermissao";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@IdFuncionario", _idFuncionario);
+                cmd.Parameters.AddWithValue("@IdPermissao", _idPermissao);
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                        return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar validar permissões do usuário no banco de dados.", ex);
             }
             finally
             {
