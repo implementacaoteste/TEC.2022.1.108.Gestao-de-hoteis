afb78a633837512453a9542c6901f72e8816f7e7
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-05-16 16:29:41 -0300

    classe Reserva

--- WindowsFormsPrincipal/Models/Reserva.cs
+++ WindowsFormsPrincipal/Models/Reserva.cs
@@ -8,6 +8,28 @@ namespace Models
 {
     public class Reserva
     {
-
+        public int Id { get; set; }
+        public int Id_Hospede { get; set; }
+        public int Id_Funcionario { get; set; }
+        public int Id_Pagamento { get; set; }
+        public int Qtd_Hospedes { get; set; }
+        public string Nome_Hospede { get; set; }
+        public string CPF_Hopesde { get; set; }
+        public string Nome_Funcionario { get; set; }
+        public string Pagamento { get; set; }
+        public string Obs_Reserva { get; set; }
+        public string Obs_Checkin { get; set; }
+        public string Obs_Checkout { get; set; }
+        public double Valor_Total { get; set; }
+        public double Valor_Entrada { get; set; }
+        public DateTime Data_Ent_Reserva { get; set; }
+        public DateTime Data_Sai_Reserva { get; set; }
+        public DateTime Data_Checkin { get; set; }
+        public DateTime Data_Checkout { get; set; }
+        public List<Quarto> Quartos { get; set; }
+        public Reserva()
+        {
+            Quartos = new List<Quarto>();
+        }
     }
 }
