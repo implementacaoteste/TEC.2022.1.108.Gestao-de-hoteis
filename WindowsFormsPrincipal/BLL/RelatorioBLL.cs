6561e5119025053b2ce0131b9e1bd6e37bee0539
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-05-16 16:13:53 -0300

    Criação do relatorio DAL e BLL

---
+++ WindowsFormsPrincipal/BLL/RelatorioBLL.cs
@@ -0,0 +1,34 @@
+﻿using DAL;
+using DALL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class RelatorioBLL
+    {
+        public List<Relatorio> BuscarPortodos()
+        {
+            return new RelatorioDAL().BuscaPorTodos();
+        }
+
+        public List<Relatorio> DataSaida(int _dataSaida)
+        {
+            return new RelatorioDAL().DataSaida(_dataSaida);
+        }
+
+        public List<Relatorio> DataEntrada(int _dataEntrada)
+        {
+            return new RelatorioDAL().DataEntrada(_dataEntrada);
+        }
+        public List<Relatorio> Datas(int _data)
+        {
+            return new RelatorioDAL().Datas(_data);
+        }
+
+    }
+}
---
+++ WindowsFormsPrincipal/DAL/RelatorioDAL.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class RelatorioDAL
+    {
+        public List<Relatorio> BuscaPorTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Relatorio> DataEntrada(int dataEntrada)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Relatorio> Datas(int data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Relatorio> DataSaida(int dataSaida)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
