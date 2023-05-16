51048ea91862edf932829137730df9c07015a17e
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-05-16 17:04:14 -0300

    Criação dos métodos da Reserva

--- WindowsFormsPrincipal/BLL/ReservaBLL.cs
+++ WindowsFormsPrincipal/BLL/ReservaBLL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,72 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class ReservaBLL
+    public class ReservaBLL
     {
+        public void Inserir(Reserva _reserva)
+        {
+            ValidarPermissao(14);
+            ValidarDados(_reserva);
+            new ReservaDAL().Inserir(_reserva);
+        }
+        public void Alterar(Reserva _reserva)
+        {
+            ValidarPermissao(15);
+            new ReservaDAL().Alterar(_reserva);
+        }
+        public void Excluir(int _id)
+        {
+            ValidarPermissao(16);
+            new ReservaDAL().Excluir(_id);
+        }
+        public void CancelarReserva(int _id)
+        {
+            ValidarPermissao(16);
+            new ReservaDAL().CancelarReserva(_id);
+        }
+        public Reserva BuscarPorId(int _id)
+        {
+            ValidarPermissao(13);
+            return new ReservaDAL().BuscarPorId(_id);
+        }
+        public List<Reserva> BuscarPorTodas()
+        {
+            ValidarPermissao(13);
+            return new ReservaDAL().BuscarPorTodas();
+        }
+        public List<Reserva> BuscarPorDataCheckin(string _dataCheckin)
+        {
+            ValidarPermissao(13);
+            return new ReservaDAL().BuscarPorDataCheckin(_dataCheckin);
+        }
+        public List<Reserva> BuscarPorDataCheckout(string _dataCheckout)
+        {
+            ValidarPermissao(13);
+            return new ReservaDAL().BuscarPorDataCheckout(_dataCheckout);
+        }
+        public List<Reserva> BuscarPorDataLancamento(string _dataLancamento)
+        {
+            ValidarPermissao(13);
+            return new ReservaDAL().BuscarPorDataLancamento(_dataLancamento);
+        }
+        public void ValidarPermissao(int _idPermissao)
+        {
+            if (!new ReservaDAL().ValidarPermissao(Constante.IdLogado, _idPermissao))
+                throw new Exception("Você não tem permissão de realizar essa operação. Procure o administrador do sistema.");
+        }
+        private void ValidarDados(Reserva _reserva)
+        {
+            if (_reserva.Data_Ent_Reserva < DateTime.Now.Date)
+                throw new Exception("Data Inválida! Você não pode colocar uma data Retroativa!");
+
+            if (_reserva.Data_Sai_Reserva < DateTime.Now.Date)
+                throw new Exception("Data Inválida! Você não pode colocar uma data Retroativa!");
+        }
+
+        public void SelecionarQuarto(int _idReserva, int _idQuarto)
+        {
+            if (!new ReservaDAL().ReservaPertenceQuarto(_idReserva, _idQuarto))
+                new ReservaDAL().SelecionarQuarto(_idReserva, _idQuarto);
+        }
     }
 }
--- WindowsFormsPrincipal/DAL/ReservaDAL.cs
+++ WindowsFormsPrincipal/DAL/ReservaDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,47 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class ReservaDAL
+    public class ReservaDAL
     {
+        public void Inserir(Reserva reserva)
+        {
+            throw new NotImplementedException();
+        }
+        public void Alterar(Reserva reserva)
+        {
+            throw new NotImplementedException();
+        }
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void CancelarReserva(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Reserva BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Reserva> BuscarPorTodas()
+        {
+            throw new NotImplementedException();
+        }
+        public List<Reserva> BuscarPorDataCheckin(string dataCheckin)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Reserva> BuscarPorDataCheckout(string dataCheckout)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Reserva> BuscarPorDataLancamento(string dataLancamento)
+        {
+            throw new NotImplementedException();
+        }
+        public bool ValidarPermissao(int idLogado, int idPermissao)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
