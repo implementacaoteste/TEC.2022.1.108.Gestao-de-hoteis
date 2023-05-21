6ba0aa2ddd444cb6d43d86245e24b2dc5cd498fc
Lucas Alves <70042204+lucasalvees@users.noreply.github.com>
2023-05-20 23:10:50 -0300

    Criação do FormReserva

---
+++ WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormReserva.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsAppGestaoHotel
+{
+    partial class FormReserva
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormReserva));
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.buttonCadastrarReserva = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Image = ((System.Drawing.Image)(resources.GetObject("buttonBuscar.Image")));
+            this.buttonBuscar.Location = new System.Drawing.Point(515, 179);
+            this.buttonBuscar.MaximumSize = new System.Drawing.Size(30, 30);
+            this.buttonBuscar.MinimumSize = new System.Drawing.Size(30, 30);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(30, 30);
+            this.buttonBuscar.TabIndex = 2;
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            // 
+            // buttonCadastrarReserva
+            // 
+            this.buttonCadastrarReserva.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(183)))), ((int)(((byte)(219)))));
+            this.buttonCadastrarReserva.FlatAppearance.BorderSize = 0;
+            this.buttonCadastrarReserva.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCadastrarReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCadastrarReserva.ForeColor = System.Drawing.Color.White;
+            this.buttonCadastrarReserva.Image = ((System.Drawing.Image)(resources.GetObject("buttonCadastrarReserva.Image")));
+            this.buttonCadastrarReserva.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonCadastrarReserva.Location = new System.Drawing.Point(12, 12);
+            this.buttonCadastrarReserva.Name = "buttonCadastrarReserva";
+            this.buttonCadastrarReserva.Size = new System.Drawing.Size(120, 33);
+            this.buttonCadastrarReserva.TabIndex = 1;
+            this.buttonCadastrarReserva.Text = "Reserva";
+            this.buttonCadastrarReserva.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonCadastrarReserva.UseVisualStyleBackColor = false;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 57);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            // 
+            // FormReserva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.buttonCadastrarReserva);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormReserva";
+            this.ShowInTaskbar = false;
+            this.Text = "HOTELOGIX - Reservas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonCadastrarReserva;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
