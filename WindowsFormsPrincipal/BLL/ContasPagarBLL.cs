e748a324d6ac075b618c359ed8f910fe1814958c
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-05-22 15:26:30 -0300

    criaçao a ContasPagar BLL e DAL

---
+++ WindowsFormsPrincipal/BLL/ContasPagarBLL.cs
@@ -0,0 +1,30 @@
+﻿using DALL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ContasPagarBLL
+    {
+        public void Inserir(ContasPagar _contasPagar)
+        {
+            new ContasPagarDLL().Inserir(_contasPagar);
+        }
+        public void Alterar(Classe _classe)
+        {
+            new ClasseDAL().Alterar(_classe);
+        }
+        public void Excluir(int _id)
+        {
+            new ClasseDAL().Excluir(_id);
+        }
+        public List<Classe> BuscaPorTodos()
+        {
+            return new ClasseDAL().BuscaPorTodos();
+        }-
+    }
+}
