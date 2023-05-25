814c09ce682e64e6884ea06379af1994ca5ab239
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-05-25 16:28:51 -0300

    bll

--- WindowsFormsPrincipal/BLL/ContasReceberBLL.cs
+++ WindowsFormsPrincipal/BLL/ContasReceberBLL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,32 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class ContasReceberBLL
+    public class ContasReceberBLL
     {
+        public void Inserir(ContasReceber _contasReceber)
+        {
+            new ContasReceberDAL().Inserir(_contasReceber);
+        }
+        public void Alterar(ContasReceber _contasReceber)
+        {
+            new ContasReceberDAL().Alterar(_contasReceber);
+        }
+        public void Excluir(int _id)
+        {
+            new ContasReceberDAL().Excluir(_id);
+        }
+        public List<ContasPagar> BuscaPorTodos()
+        {
+            return new ContasReceberDAL().BuscaPorTodos();
+        }
+
+        public List<ContasPagar> BuscarPorData(DateTime _data)
+        {
+            return new ContasReceberDAL().BuscarPorData(_data);
+        }
+        public List<ContasPagar> BuscarPorPagar(bool _pagar)
+        {
+            return new ContasReceberDAL().BuscarPorPagar(_pagar);
+        }
     }
 }
