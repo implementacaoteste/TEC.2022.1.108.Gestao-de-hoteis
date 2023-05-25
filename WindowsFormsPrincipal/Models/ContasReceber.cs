f8e23783d09fc880cbbd858aba927882027c15c4
Klauber Maciel <115495451+KlauberMaciel@users.noreply.github.com>
2023-05-25 15:12:07 -0300

    models ContasReceber

---
+++ WindowsFormsPrincipal/Models/ContasReceber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ContasReceber
+    {
+        public int Id { get; set; }
+        public int Id_Cliente { get; set; }
+        public int Id_Funcionario { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public DateTime Data_Vencimento { get; set; }
+        public bool Receber { get; set; }
+        public string Nome_Hospede { get; set; }
+        public string Nome_funcionario { get; set; }
+    }
+}
