0dc20a027c08644d8e6646987892fe795a7ce95c
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-05-26 16:47:09 -0300

    Criação do project Infra e a classe Criptografia

---
+++ WindowsFormsPrincipal/Infra/Criptografia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra
+{
+    public class Criptografia
+    {
+        public string CriptografarSenha(string _senha)
+        {
+            string retorno = _senha;
+
+            for (int i = 0; i< _senha.Length; i++) 
+            {
+                retorno = GerarHash(retorno);
+                retorno = ReverterTexto(retorno);
+                retorno = GerarHash(retorno);
+                retorno = ReverterTexto(retorno);
+            }
+            return GerarHash(retorno);
+        }
+        private string GerarHash(string _texto)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(_texto));
+                return Convert.ToBase64String(hashedBytes);
+            }
+        }
+        private string ReverterTexto(string _texto)
+        {
+            char[] charArray = _texto.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
