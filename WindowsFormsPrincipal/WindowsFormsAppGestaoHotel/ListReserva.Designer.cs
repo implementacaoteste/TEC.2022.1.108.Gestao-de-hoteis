5eff155c87984d3f89acdb9c671f8fb70e848929
Lucas Alves <70042204+lucasalvees@users.noreply.github.com>
2023-05-26 12:04:51 -0300

    Adiciona ListReserva

---
+++ WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/ListReserva.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace WindowsFormsAppGestaoHotel
+{
+    partial class ListReserva
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ListReserva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ListReserva";
+            this.Size = new System.Drawing.Size(180, 120);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
