6fab432fa9631da4890104970d56dbd1baba5499
Lucas Alves <70042204+lucasalvees@users.noreply.github.com>
2023-05-31 17:08:36 -0300

    Correção da StringDeConexao

--- WindowsFormsPrincipal/DAL/Conexao.cs
+++ WindowsFormsPrincipal/DAL/Conexao.cs
@@ -11,8 +11,8 @@ namespace DAL
             get
             {
                 if (string.IsNullOrEmpty(stringDeConexao))
-                    if (File.Exists("C:\\ConfiguracaoBanco\\Conexao.config"))
-                        stringDeConexao = File.ReadAllText("C:\\ConfiguracaoBanco\\Conexao.config");
+                    if (File.Exists("C:\\Configuracao\\Conexao.config"))
+                        stringDeConexao = File.ReadAllText("C:\\Configuracao\\Conexao.config");
                     else throw new Exception("Você precisa criar a conexão com o Banco de Dados");
 
                 return stringDeConexao;
