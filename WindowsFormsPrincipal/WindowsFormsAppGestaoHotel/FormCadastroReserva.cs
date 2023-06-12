2bce5adb1007400e6f2f268db3401be0b1fd60c1
Lucas Alves <70042204+lucasalvees@users.noreply.github.com>
2023-06-12 16:05:52 -0300

    Alteração no Designer do FormConsultaQuarto

--- WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormCadastroReserva.cs
+++ WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormCadastroReserva.cs
@@ -142,7 +142,6 @@ namespace WindowsFormsAppGestaoHotel
 
                     }
                 }
-                //CalcularValorReserva();
             }
             catch (Exception ex)
             {
@@ -165,18 +164,18 @@ namespace WindowsFormsAppGestaoHotel
             DateTime data_ent = data_Ent_ReservaDateTimePicker.Value;
             DateTime data_sai = data_SaidaDateTimePicker.Value;
             int TotalDays = (data_sai.Date - data_ent.Date).Days;
-            double valor_total = 0;
+            //double valor_total = 0;
             //double valor_total_reserva = ((Reserva)reservaBindingSource.Current).Valor_Total;
-            double valor_diaria = ((Quarto)quartosBindingSource.Current).Valor_Diaria;
+            //double valor_diaria = ((Quarto)quartosBindingSource.Current).Valor_Diaria;
 
             
 
-            if (valor_diaria > 0)
+            /*if (valor_diaria > 0)
                 valor_total = TotalDays * valor_diaria;
 
             valor_EntradaTextBox.Text = (valor_total / 2).ToString();
 
-            valor_TotalTextBox.Text = valor_total.ToString();
+            valor_TotalTextBox.Text = valor_total.ToString();*/
 
             //if (Double.TryParse(valor_DiariaTextBox.Text, out valor_diaria))
             //    valor_DiariaTextBox.Text = valor_diaria.ToString("C", CultureInfo.CurrentCulture);
@@ -213,14 +212,14 @@ namespace WindowsFormsAppGestaoHotel
 
         private void data_Ent_ReservaDateTimePicker_ValueChanged(object sender, EventArgs e)
         {
-            valor_EntradaTextBox.Focus();
-            data_Ent_ReservaDateTimePicker.Focus();
+            /*valor_EntradaTextBox.Focus();
+            data_Ent_ReservaDateTimePicker.Focus();*/
         }
 
         private void data_SaidaDateTimePicker_ValueChanged(object sender, EventArgs e)
         {
-            valor_EntradaTextBox.Focus();
-            data_Ent_ReservaDateTimePicker.Focus();
+            /*valor_EntradaTextBox.Focus();
+            data_Ent_ReservaDateTimePicker.Focus();*/
         }
 
         private void valor_EntradaTextBox_Enter(object sender, EventArgs e)
--- WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormConsultaQuarto.Designer.cs
+++ WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/FormConsultaQuarto.Designer.cs
@@ -30,9 +30,17 @@
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.Label labelQuarto;
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConsultaQuarto));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.quartoDataGridView = new System.Windows.Forms.DataGridView();
+            this.quartoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonSelecionar = new System.Windows.Forms.Button();
+            this.buttonBuscar = new System.Windows.Forms.Button();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Andar = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -40,10 +48,6 @@
             this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.quartoBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.buttonCancelar = new System.Windows.Forms.Button();
-            this.buttonSelecionar = new System.Windows.Forms.Button();
-            this.buttonBuscar = new System.Windows.Forms.Button();
             labelQuarto = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.quartoDataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.quartoBindingSource)).BeginInit();
@@ -58,13 +62,14 @@
             labelQuarto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             labelQuarto.Name = "labelQuarto";
             labelQuarto.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            labelQuarto.Size = new System.Drawing.Size(743, 53);
+            labelQuarto.Size = new System.Drawing.Size(743, 75);
             labelQuarto.TabIndex = 0;
             labelQuarto.Text = "Consultar Quarto";
             labelQuarto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // textBox1
             // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.818182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(11, 82);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
@@ -93,77 +98,11 @@
             this.quartoDataGridView.Margin = new System.Windows.Forms.Padding(2);
             this.quartoDataGridView.Name = "quartoDataGridView";
             this.quartoDataGridView.ReadOnly = true;
+            this.quartoDataGridView.RowHeadersVisible = false;
             this.quartoDataGridView.RowHeadersWidth = 51;
             this.quartoDataGridView.RowTemplate.Height = 24;
             this.quartoDataGridView.Size = new System.Drawing.Size(720, 259);
             this.quartoDataGridView.TabIndex = 4;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
-            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
-            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            this.dataGridViewTextBoxColumn1.ReadOnly = true;
-            this.dataGridViewTextBoxColumn1.Width = 60;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.DataPropertyName = "Numero";
-            this.dataGridViewTextBoxColumn2.HeaderText = "Número";
-            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            this.dataGridViewTextBoxColumn2.ReadOnly = true;
-            this.dataGridViewTextBoxColumn2.Width = 90;
-            // 
-            // Andar
-            // 
-            this.Andar.DataPropertyName = "Andar";
-            this.Andar.HeaderText = "Andar";
-            this.Andar.MinimumWidth = 6;
-            this.Andar.Name = "Andar";
-            this.Andar.ReadOnly = true;
-            this.Andar.Width = 125;
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            this.dataGridViewTextBoxColumn4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dataGridViewTextBoxColumn4.DataPropertyName = "Descricao";
-            this.dataGridViewTextBoxColumn4.FillWeight = 111.9789F;
-            this.dataGridViewTextBoxColumn4.HeaderText = "Descrição";
-            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            this.dataGridViewTextBoxColumn4.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn5
-            // 
-            this.dataGridViewTextBoxColumn5.DataPropertyName = "Valor_Diaria";
-            this.dataGridViewTextBoxColumn5.FillWeight = 11.77997F;
-            this.dataGridViewTextBoxColumn5.HeaderText = "Valor da Diaria";
-            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
-            this.dataGridViewTextBoxColumn5.ReadOnly = true;
-            this.dataGridViewTextBoxColumn5.Width = 115;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.DataPropertyName = "Classe";
-            this.dataGridViewTextBoxColumn3.FillWeight = 51.85076F;
-            this.dataGridViewTextBoxColumn3.HeaderText = "Classe";
-            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            this.dataGridViewTextBoxColumn3.ReadOnly = true;
-            this.dataGridViewTextBoxColumn3.Width = 120;
-            // 
-            // dataGridViewTextBoxColumn6
-            // 
-            this.dataGridViewTextBoxColumn6.DataPropertyName = "Status";
-            this.dataGridViewTextBoxColumn6.FillWeight = 224.3901F;
-            this.dataGridViewTextBoxColumn6.HeaderText = "Status";
-            this.dataGridViewTextBoxColumn6.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
-            this.dataGridViewTextBoxColumn6.ReadOnly = true;
-            this.dataGridViewTextBoxColumn6.Width = 80;
             // 
             // quartoBindingSource
             // 
@@ -220,6 +159,82 @@
             this.buttonBuscar.UseVisualStyleBackColor = false;
             this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
             // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.dataGridViewTextBoxColumn1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Cód";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 80;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Numero";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.dataGridViewTextBoxColumn2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewTextBoxColumn2.HeaderText = "Número";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // Andar
+            // 
+            this.Andar.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Andar.DataPropertyName = "Andar";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.Andar.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Andar.HeaderText = "Andar";
+            this.Andar.MinimumWidth = 6;
+            this.Andar.Name = "Andar";
+            this.Andar.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Descricao";
+            this.dataGridViewTextBoxColumn4.FillWeight = 111.9789F;
+            this.dataGridViewTextBoxColumn4.HeaderText = "Descrição";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Valor_Diaria";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle4.Format = "C2";
+            dataGridViewCellStyle4.NullValue = null;
+            this.dataGridViewTextBoxColumn5.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridViewTextBoxColumn5.FillWeight = 150F;
+            this.dataGridViewTextBoxColumn5.HeaderText = "Diária";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Classe";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Classe";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "Status";
+            this.dataGridViewTextBoxColumn6.HeaderText = "Status";
+            this.dataGridViewTextBoxColumn6.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
             // FormConsultaQuarto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -240,6 +255,7 @@
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(759, 469);
             this.Name = "FormConsultaQuarto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consulta de Quarto";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormConsultaQuarto_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.quartoDataGridView)).EndInit();
