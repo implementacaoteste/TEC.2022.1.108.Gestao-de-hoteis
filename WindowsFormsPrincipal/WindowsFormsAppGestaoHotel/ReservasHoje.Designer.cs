98940d0b39f96d6e3e2f2c7e1e294166ef5ce6d0
Lucas Alves <70042204+lucasalvees@users.noreply.github.com>
2023-07-11 02:11:22 -0300

    Criação do UserControl ReservasHoje

---
+++ WindowsFormsPrincipal/WindowsFormsAppGestaoHotel/ReservasHoje.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsAppGestaoHotel
+{
+    partial class ReservasHoje
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReservasHoje));
+            this.qtdReservasLabel = new System.Windows.Forms.Label();
+            this.labelCheckIn = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.reservaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reservaBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // qtdReservasLabel
+            // 
+            this.qtdReservasLabel.AutoSize = true;
+            this.qtdReservasLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qtdReservasLabel.Location = new System.Drawing.Point(113, 7);
+            this.qtdReservasLabel.Name = "qtdReservasLabel";
+            this.qtdReservasLabel.Size = new System.Drawing.Size(20, 24);
+            this.qtdReservasLabel.TabIndex = 3;
+            this.qtdReservasLabel.Text = "0";
+            // 
+            // labelCheckIn
+            // 
+            this.labelCheckIn.AutoSize = true;
+            this.labelCheckIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCheckIn.Location = new System.Drawing.Point(67, 33);
+            this.labelCheckIn.Name = "labelCheckIn";
+            this.labelCheckIn.Size = new System.Drawing.Size(113, 20);
+            this.labelCheckIn.TabIndex = 4;
+            this.labelCheckIn.Text = "Reservas Hoje";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 52);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // reservaBindingSource
+            // 
+            this.reservaBindingSource.DataSource = typeof(Models.Reserva);
+            // 
+            // ReservasHoje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.qtdReservasLabel);
+            this.Controls.Add(this.labelCheckIn);
+            this.Name = "ReservasHoje";
+            this.Size = new System.Drawing.Size(200, 60);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reservaBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label qtdReservasLabel;
+        private System.Windows.Forms.Label labelCheckIn;
+        private System.Windows.Forms.BindingSource reservaBindingSource;
+    }
+}
